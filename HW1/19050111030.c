22520816bf3fa4315f06ef864959c10f83f64123
Betül KARAMAN <btlkrmn220@gmail.com>
2023-03-19 17:27:19 +0300

    hw2
    perform matrix-vector multiplication operations with that matrix

--- HW1/19050111030.c
+++ HW1/19050111030.c
@@ -1,8 +1,60 @@
-#include <hellomake.h>
+#include "hellomake.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
-int main(){
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        printf("Usage: %s <matrix_rows> <matrix_cols> <output_filename>\n", argv[0]);
+        return 1;
+    }
 
-	myfunction();
-	return 0;
 
+    int matrix_rows = atoi(argv[1]);
+    int matrix_cols = atoi(argv[2]);
+    char *output_filename = argv[3];
+
+ 
+    srand(time(NULL));
+
+    // Allocate memory
+    double **matrix = (double **)malloc(matrix_rows * sizeof(double *));
+    for (int i = 0; i < matrix_rows; i++) {
+        matrix[i] = (double *)calloc(matrix_cols, sizeof(double));
+        for (int j = 0; j < matrix_cols; j++) {
+            // Fill the matrix with random double values between 1.0 and 100.0
+            matrix[i][j] = ((double)rand() / RAND_MAX) * 99.0 + 1.0;
+        }
+    }
+
+    // Allocate memory
+    double *vector = (double *)calloc(matrix_cols, sizeof(double));
+    for (int i = 0; i < matrix_cols; i++) {
+
+        vector[i] = ((double)rand() / RAND_MAX) * 99.0 + 1.0;
+    }
+
+    // multiplication
+    double *result = (double *)calloc(matrix_rows, sizeof(double));
+    for (int i = 0; i < matrix_rows; i++) {
+        for (int j = 0; j < matrix_cols; j++) {
+            result[i] += matrix[i][j] * vector[j];
+        }
+    }
+
+    FILE *output_file = fopen(output_filename, "w");
+    for (int i = 0; i < matrix_rows; i++) {
+        fprintf(output_file, "%.2f\n", result[i]);
+    }
+    fclose(output_file);
+
+
+    for (int i = 0; i < matrix_rows; i++) {
+        free(matrix[i]);
+    }
+    free(matrix);
+    free(vector);
+    free(result);
+
+    return 0;
 }
